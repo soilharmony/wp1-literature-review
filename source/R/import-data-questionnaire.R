21c61a28c32ac611f3a04199e1d559df42bd27c1
Ben Serrien <ben.serrien@inbo.be>
2026-07-02 16:39:46 +0200

    init script/quarto

--- source/R/import-data-questionnaire.R
+++ source/R/import-data-questionnaire.R
@@ -1,21 +1,18 @@
-library(tidyverse)
-library(googlesheets4)
-
-
-url_sheet <- paste0("https://docs.google.com/spreadsheets/d/",
-                    "13Qcj2Eg_oI9aUy7vL_34XdF-oucMppBdhk2w70AQMHk/",
-                    "edit?usp=sharing")
-
-
-data_wp1q <- read_sheet(
-  url_sheet, sheet = "general",
-  range = "A2:C16", col_names = F
-) 
-
-q_labels  <- data_wp1q[, 1]
-q_names   <- data_wp1q[, 2] %>% pull()
-q_answers <- data_wp1q[, 3:ncol(data_wp1q)]
-
-q_answers <- as.data.frame(unname(t(q_answers)))
-colnames(q_answers) <- q_names
-
+#' import_data_questionaire
+#' 
+#' @param gsheet URL to the google sheet of the questionaire
+#' 
+import_data_questionaire <- function(gsheet) {
+  # sheet with general information
+  data_general <- read_sheet(
+    gsheet, sheet = "general",
+    range = "A2:C16", col_names = F
+  )
+  # transpose to tidy format
+  q_labels  <- data_general[, 1]
+  q_names   <- data_general[, 2] %>% pull()
+  q_answers <- data_general[, 3:ncol(data_general)]
+  q_answers <- as.data.frame(unname(t(q_answers)))
+  colnames(q_answers) <- q_names
+  return(q_answers)
+}
