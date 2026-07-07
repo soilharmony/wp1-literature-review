762805703014f0c71f2f33e2fdd6b178801fb317
Ben Serrien <ben.serrien@inbo.be>
2026-07-07 14:19:28 +0200

    changes to new googlesheet format

--- source/R/import-data-questionnaire.R
+++ source/R/import-data-questionnaire.R
@@ -7,17 +7,25 @@ import_data_questionaire <- function(gsheet) {
   # sheet with general information (step 1)
   data_general <- read_sheet(
     gsheet, sheet = "generalQ",
-    col_names = F
+    # import without header, assign later
+    col_names = F,   
+    # safest to import all cols as character, pre-process later to other type
+    col_types = "c"  
   )
   
-  # row 2 in the spreadsheet = row 1 in data.frame = question labels
-  col_names  <- as.character(data_general[1, ])
-  col_labels <- as.character(data_general[2, ])
+  # specific rows in the spreadsheet
+  col_names     <- as.character(data_general[1, ])
+  col_class     <- as.character(data_general[2, ])
+  col_labels    <- as.character(data_general[3, ])
+  col_info      <- as.character(data_general[4, ])
+  col_mandatory <- as.character(data_general[5, ])
   
-  data_general <- data_general[-(1:2), ]
+  data_general <- data_general[-(1:5), ]
   names(data_general) <- col_names
   for (i in seq_along(data_general)) {
-    attr(data_general[[i]], "label") <- col_labels[i]
+    attr(data_general[[i]], "label")     <- col_labels[i]
+    attr(data_general[[i]], "mandatory") <- col_mandatory[i]
+    attr(data_general[[i]], "qclass")    <- col_class[i]
   }
   
   return(data_general)
